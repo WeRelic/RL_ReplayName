0dadc13b4ae06a0370c78de153c70c828c6641b9
WeRelic <31648868+WeRelic@users.noreply.github.com>
2017-12-17 21:05:28 -0500

    Taking a break.
    Currently broken. I think the problem exists around 80-89

--- NameReplacer.py
+++ NameReplacer.py
@@ -28,7 +28,7 @@
 """
 
 
-import os, sys, json, subprocess, ntpath
+import os, sys, json, subprocess, ntpath, re
 
 rattlepath = os.path.join( os.getcwd(), "win_rattle.exe" )
 
@@ -48,13 +48,13 @@ def CheckOutputDir():
         os.makedirs( os.path.join( os.getcwd(), "out" ) )
     
 
-def ReplaceNames( data ):
+def ReplaceNames( data, json_datafile ):
     """ Replace all names with generic names. """
     print( "Replacing names..." )
     reviewee_team = None
     teammate_counter = 1
     opponent_counter = 1
-    
+    index = 0
     # Find reviewee's teamID:
     for player in data['header']['properties']['value']['PlayerStats']['value']['array_property']:
         name = player['value']['Name']['value']['str_property']
@@ -63,20 +63,31 @@ def ReplaceNames( data ):
             
     # Iterate names, renaming players based on their team.
     for player in data['header']['properties']['value']['PlayerStats']['value']['array_property']:
+        # Get the current player's name. 
         name = player['value']['Name']['value']['str_property']
+                
         if name == FindReviewee(data):
-            print( "{} -> {}".format( name, "Reviewee" ) )
-            player['value']['Name']['value']['str_property'] = "Reviewee"
+            newname = "Reviewee"
         else:
             if player['value']['Team']['value']['int_property'] == reviewee_team:
-                print( "{} -> {}".format( name, "Teammate #{}".format( teammate_counter ) ) )
-                player['value']['Name']['value']['str_property'] = "Teammate #{}".format( teammate_counter )
+                newname = "Teammate #{}".format( teammate_counter )
                 teammate_counter += 1
             else:
-                print( "{} -> {}".format( name, "Opponent #{}".format( opponent_counter ) ) )
-                player['value']['Name']['value']['str_property'] = "Opponent #{}".format( opponent_counter )
+                newname = "Opponent #{}".format( opponent_counter )
                 opponent_counter += 1
-    return data
+
+        print( "{} -> {}".format( name, newname ) )
+
+        # Problem is in this block!
+        #json_datafile = ReplaceName( json_datafile, name, newname )
+        replacement = "{'kind': 'StrProperty', 'size': " + str( len( newname ) + 5 )
+        replacement = replacement + ", 'value': {'str_property': '" + newname + "'}}"
+        print( str(player['value']['Name']) + " gets replaced by " + replacement )
+        json_datafile = json_datafile.replace( str(player['value']['Name']), replacement )
+
+        
+        index += 1
+    return (data, json_datafile)
     
 
 def ReplayToJSON( replay ):
@@ -95,8 +106,6 @@ def JSONtoReplay( data, json_path, old_replay_path ):
     old_path = os.path.split(old_replay_path)[0]
     new_replay_path = os.path.join( old_path, file_name )
     print( "Re-encoding {} to {}...".format( json_path, new_replay_path ) )
-    with open( json_path, 'w' ) as f:
-        f.write( json.dumps( data ) )
     os.system( "{} encode {} > {}".format( rattlepath, json_path, new_replay_path ) )
     print( "Done..." )
 
@@ -108,15 +117,15 @@ def GetPlayerNames( data ):
     return [ key['value']['Name']['value']['str_property'] for key in data['header']['properties']['value']['PlayerStats']['value']['array_property'] ]
 
 
-def RenameReplay( data ):
+def RenameReplay( data, datafile ):
     """ Append "_names_fixed" to the replay file's name. """
     print( "Renaming replay from '{0}' to '{0}_names_fixed'...".format( data['header']['properties']['value']['ReplayName']['value']['str_property'] ) )
     name = data['header']['properties']['value']['ReplayName']['value']['str_property']
-    data['header']['properties']['value']['ReplayName']['value']['str_property'] = "{}_names_fixed".format( name )
-    return data
+    datafile = datafile.replace( name, "{}_names_fixed".format( name ) )
+    return (data, datafile)
     
-def ParseArgs():
-    """ Parse the command line arguments. """
+    
+if __name__ == "__main__":
     args = sys.argv[1:]
     if len( args ) < 1:
         raise SyntaxError( "There must be at least one argument. None passed." )
@@ -124,30 +133,16 @@ def ParseArgs():
     for arg in args:
         json_file = ReplayToJSON( arg )
         with open( json_file, 'r' ) as f:
-            json_data = json.load(f)
-            json_data = ReplaceNames( json_data )
-            json_data = RenameReplay( json_data )
-            JSONtoReplay( json_data, json_file, arg )
-    
-if __name__ == "__main__":
-    ParseArgs()
-##    replay_path = r"C:\Relic\Portfolio\RL_ReplayExtractor\Testing\AirTackleReplay.replay"
-##
-##    # Parsing .REPLAY to .JSON
-##    ReplayToJSON( replay_path )
-##
-##    print( "Loading decoded JSON file..." )
-##    with open( os.path.join( os.getcwd(), 'out', StripPathToFile( replay_path ).split(".")[0] + ".json" ) ) as f:
-##        data = json.load( f )
-##
-##
-##    print( FindReviewee( data ) )
-##    print( GetPlayerNames( data ) )
-##    ReplaceNames( data )
-##    print( GetPlayerNames( data ) )
-##    print( "Original Name: {}".format( data['header']['properties']['value']['ReplayName']['value']['str_property'] ) )
-##    RenameReplay( data )
-##    print( "New Replay Name: {}".format( data['header']['properties']['value']['ReplayName']['value']['str_property'] ) )
-##    pass
+            json_datafile = f.read()
+        json_data = json.loads(json_datafile)
+        json_data, json_datafile = ReplaceNames( json_data, json_datafile )
+        json_data, json_datafile = RenameReplay( json_data, json_datafile )
+        JSONtoReplay( json_data, json_file, arg )
+        
 
     
+    with open( json_file ) as f:
+        filedata = json.load(f)
+    for i in [ key['value']['Name']['value']['str_property'] for key in filedata['header']['properties']['value']['PlayerStats']['value']['array_property'] ]:
+        print( i )
+        
